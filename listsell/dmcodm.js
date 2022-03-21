a32a6ae5e442c75bac841ef0dfb19d7eeb800a83
Gilsbot88 <101207495+Gilsbot88@users.noreply.github.com>
2022-03-21 21:59:12 +0700

    Update dmcodm.js

--- listsell/dmcodm.js
+++ listsell/dmcodm.js
@@ -1,12 +1,12 @@
 module.exports.dmcodm =
-`*_Hai Kak Saya KuzaStore_*
+`*_Hai Kak Saya Ragil Store Bot_*
 _Berikut List Untuk Diamond Codm Ya Kak_
 
-● 26 💎 Rp5.075
-● 53 💎 Rp9.950
-● 106 💎 Rp19.700
-● 264 💎 Rp48.950
-● 528 💎 Rp97.700
-● 1056 💎 Rp195.200
+● 26 💎 Rp10.075
+● 53 💎 Rp15.950
+● 106 💎 Rp25.700
+● 264 💎 Rp50.950
+● 528 💎 Rp98.700
+● 1056 💎 Rp196.200
 
 _Selengkapnya Silahkan Chat Ke Owner Ya Kak_`
