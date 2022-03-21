e0b7713805828a6f6747236c6cced0a1f1afbf05
Gilsbot88 <101207495+Gilsbot88@users.noreply.github.com>
2022-03-21 22:00:37 +0700

    Update dmhigsdomino.js

--- listsell/dmhigsdomino.js
+++ listsell/dmhigsdomino.js
@@ -1,9 +1,9 @@
 module.exports.dmhigsdomino =
-`_Hai Kak Saya KuzaStore_*
+`_Hai Kak Saya Ragil Store Bot_*
 _Berikut List Untuk Diamond HigsDomino Ya Kak_
 
-● 1M   💎 Rp500
-● 60M  💎 Rp5.850
+● 1M   💎 Rp3000
+● 60M  💎 Rp10.850
 ● 200M 💎 Rp18.500
 ● 400M 💎 Rp35.500
 ● 1B    💎 Rp67.500
